6fa0324c46043f2b9cb09dc7356065a1ff1c0723
tkCreate <tkCreate@users.noreply.github.com>
2020-05-26 21:24:06 +0800

    ups
    dsfsdf

--- js/utility.js
+++ js/utility.js
@@ -1,66 +1,68 @@
 // Get the current year for the copyright
 $('#year').text(new Date().getFullYear());
 
-$('body').scrollspy({target: '#main-nav'});
+$('body').scrollspy({ target: '#main-nav' });
 
 // Scrolling
-$('#main-nav a').on('click', function (e) {
-    if (this.hash !== '') {
-        e.preventDefault();
+$('#main-nav a').on('click', function(e) {
+	if (this.hash !== '') {
+		e.preventDefault();
 
-        const hash = this.hash;
+		const hash = this.hash;
 
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 900, function () {
-            window.location.hash = hash;
-        });
-
-    }
+		$('html, body').animate(
+			{
+				scrollTop: $(hash).offset().top
+			},
+			900,
+			function() {
+				window.location.hash = hash;
+			}
+		);
+	}
 });
 
 // SCROLL
-window.onscroll = function () {
-    // When the user scrolls down 100px from the top of the document, show the button
-    scrollFunction()
-    //hide navbar
-    hideNav()
+window.onscroll = function() {
+	// When the user scrolls down 100px from the top of the document, show the button
+	scrollFunction();
+	//hide navbar
+	hideNav();
 };
 
 //HIDE navbar
 var prevScrollpos = window.pageYOffset;
 
 function hideNav() {
-    var currentScrollpos = window.pageYOffset;
-    if (prevScrollpos > currentScrollpos) {  //up
-        $("#main-nav").css({
-            display: 'none',
-        })
-    } else {  //down
-        $("#main-nav").css({
-            display: 'block',
-        })
-    }
+	var currentScrollpos = window.pageYOffset;
+	if (prevScrollpos > currentScrollpos) {
+		//up
+		$('#main-nav').css({
+			display: 'none'
+		});
+	} else {
+		//down
+		$('#main-nav').css({
+			display: 'block'
+		});
+	}
 
-    prevScrollpos = currentScrollpos;
+	prevScrollpos = currentScrollpos;
 }
 
 //Scroll to TOP
 function scrollFunction() {
-    if (document.documentElement.scrollTop > 100) {
-        $("#topUp").css({
-            display: 'block',
-        })
-    } else {
-        $("#topUp").css({
-            display: 'none',
-        })
-    }
+	if (document.documentElement.scrollTop > 100) {
+		$('#topUp').css({
+			display: 'block'
+		});
+	} else {
+		$('#topUp').css({
+			display: 'none'
+		});
+	}
 }
 
-$('#topUp').click(function () {
-    // just smooth scroll
-    $('html').animate({
-        scrollTop: 0
-    }, 900);
-})
+$('#topUp').click(function() {
+	document.documentElement.scrollTop = 0;
+});
